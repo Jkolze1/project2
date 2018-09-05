c459604ec43ad2c42fd39cdacd9a53a879139d5c
Jacqueline Kolze <38360663+Jkolze1@users.noreply.github.com>
2018-09-05 16:50:09 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var db = require("./models");
 // }
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 8000
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -44,14 +44,9 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+server.listen(port, function() {
+  console.log("App is running on port " + port);
 });
+ 
 
 module.exports = app;
